0064059d8ae869308c2db9021a3fa73bd3f3549c
Timothy Burnsed <143354694+tburnsed@users.noreply.github.com>
2023-08-30 09:53:21 -0400

    Original File Upload
    Removing any old data

---
+++ DisableBitLockerAndUpdate-PS-1.ps1
@@ -0,0 +1,32 @@
+﻿#start logging
+$name = hostname
+$date = get-date -f yyyy-MM-dd----hh-mm_ss
+$filename = $name + "--" +  $date +".txt"
+$filepath = "\\192.168.80.20\it\UpgradeLogs\"+$filename
+$filepath
+New-Item $filepath
+
+
+#Disable BL
+write-host "current status:" | Add-Content -Path $filepath
+$status = Get-BitLockerVolume -MountPoint 'C:' | Select-Object -ExpandProperty Protectionstatus | Add-Content -Path $filepath
+write-host "Running unlock command...." | Add-Content -Path $filepath
+manage-bde -protectors -disable C: | Add-Content -Path $filepath
+
+#Check if disabled and run update
+write-host "Re-checking lock status...." | Add-Content -Path $filepath
+$status = Get-BitLockerVolume -MountPoint 'C:' | Select-Object -ExpandProperty Protectionstatus | Add-Content -Path $filepath
+
+
+if ($status -eq "off")
+        {write-host "protection off running update" | Add-Content -Path $filepath
+        Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=799445 -OutFile C:\Windows\Temp\Windows10Upgrade9252.exe | Add-Content -Path $filepath
+        C:\Windows\Temp\Windows10Upgrade9252.exe /quietinstall /skipeula /auto upgrade  | Add-Content -Path $filepath
+        
+       }
+
+else { write-host "Error. protection still on exiting" | Add-Content -Path $filepath
+       exit 
+       
+       }
+
